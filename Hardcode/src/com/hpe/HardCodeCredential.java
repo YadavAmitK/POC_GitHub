248c27fae898f696187261bd914762c410c6a283
Kumar Yadav <amit.kumar-yadav@hpe.com>
2023-08-16 13:32:36 +0530

    java standalone code

---
+++ Hardcode/src/com/hpe/HardCodeCredential.java
@@ -0,0 +1,31 @@
+package com.hpe;
+
+import java.util.Scanner;
+
+public class HardCodeCredential {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String Username;
+        String Password;
+        Password = "admin";
+        Username = "admin";
+        Scanner sc1 = new Scanner(System.in);       
+        System.out.println("Enter Username : ");      
+        String username = sc1.next();      
+        Scanner sc2 = new Scanner(System.in);      
+        System.out.println("Enter Password : ");
+        String password = sc2.next();       
+        if (username.equals(Username) && password.equals(Password)) {
+            System.out.println("Access Granted!");
+        } else if (username.equals(Username)) {
+            System.out.println("Invalid Password!");
+        } else if (password.equals(Password)) {
+            System.out.println("Invalid Username!");
+        } else {
+            System.out.println("Invalid Username & Password!");
+        }
+
+	}
+
+}
