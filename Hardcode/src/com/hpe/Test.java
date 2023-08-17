cf22b5d9afa27cb27ec8b95f04ecb65876e87658
Kumar Yadav <amit.kumar-yadav@hpe.com>
2023-08-17 09:43:59 +0530

    test class

---
+++ Hardcode/src/com/hpe/Test.java
@@ -0,0 +1,34 @@
+package com.hpe;
+
+import java.util.Scanner;
+/*
+java code.........
+*/
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    String Username;
+        String Password;
+        Password = "admin5";
+        Username = "admin5";
+        Scanner sc1 = new Scanner(System.in);       
+        System.out.println("Enter Username : ");      
+        String username = sc1.next();      
+        Scanner sc2 = new Scanner(System.in);      
+        System.out.println("Enter Password : ");
+        String password = sc2.next();       
+        if (username.equals(Username) && password.equals(Password)) {
+            System.out.println("Access Granted!");
+        } else if (username.equals(Username)) {
+            System.out.println("Invalid Password!");
+        } else if (password.equals(Password)) {
+            System.out.println("Invalid Username!");
+        } else {
+            System.out.println("Invalid Username & Password!");
+        }
+
+	}
+
+}
